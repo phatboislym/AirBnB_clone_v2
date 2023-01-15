a2e948c4df166151b96cf5d5cd3a602083f5061e
Adewole <phatboislym@protonmail.com>
2023-01-16 00:34:54 +0100

    fix: add documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+a script that starts a Flask web application
+Requirements:
+web application must be listening on 0.0.0.0, port 5000
+Routes:
+/: display “Hello HBNB!”
+must use the option strict_slashes=False in your route definition
+"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,6 +14,10 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def home():
+    """
+    Route:
+    /: displays “Hello HBNB!”
+    """
     return (f"Hello HBNB!")
 
 
